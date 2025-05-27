62c7fa78ba755ec03b32bbe4f58c6d705883c121
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-27 14:55:29 +0600

    class add

--- src/componenta/Register/Register.jsx
+++ src/componenta/Register/Register.jsx
@@ -1,13 +1,21 @@
 const Register = () => {
   return (
     <div>
-      <h2 className="text-3xl">Please Register</h2>
-      <form>
-        <input type="email" name="email" id="" />
-        <br />
-        <input type="password" name="password" id="" />
-        <input type="submit" value="Register" />
-      </form>
+      <div className="mx-auto md:w-1/2">
+        <h2 className="text-3xl">Please Register</h2>
+        <form>
+          <input className="bg-gray-300 my-4" type="email" name="email" id="" />
+          <br />
+          <input
+            className="bg-gray-300"
+            type="password"
+            name="password"
+            id=""
+          />
+          <br />
+          <input type="submit" value="Register" />
+        </form>
+      </div>
     </div>
   );
 };
