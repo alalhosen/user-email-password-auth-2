5ba0640832e1446624bec2e533fc97d95d2ca69d
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-27 10:54:43 +0600

    daisyui change to email auth

--- src/componenta/Header/Header.jsx
+++ src/componenta/Header/Header.jsx
@@ -36,14 +36,12 @@ const Header = () => {
             {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Email Auth</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navLinks}</ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
+     
     </div>
   );
 };
