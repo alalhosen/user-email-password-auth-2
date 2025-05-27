a0aec073291f302d6858439ab0f1af5a4ff748d6
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-27 11:23:22 +0600

    create form

--- src/componenta/Register/Register.jsx
+++ src/componenta/Register/Register.jsx
@@ -2,6 +2,12 @@ const Register = () => {
   return (
     <div>
       <h2 className="text-3xl">Please Register</h2>
+      <form>
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="password" name="password" id="" />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
