500ec0882dda3a4fa3bd329884c4d58608df8d5f
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-31 11:28:21 +0600

    onSubmit

--- src/componenta/HeroRegister/HeroRegister.jsx
+++ src/componenta/HeroRegister/HeroRegister.jsx
@@ -1,5 +1,11 @@
-
 const HeroRegister = () => {
+
+
+const handleRegister = e => {
+    e.preventDefault();
+    console.log('form submitted')
+}
+
   return (
     <div>
       <div className="hero bg-base-200 min-h-screen">
@@ -14,7 +20,8 @@ const HeroRegister = () => {
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-              <fieldset className="fieldset">
+           <form onSubmit={handleRegister}>
+                 <fieldset className="fieldset">
                 <label className="label">Email</label>
                 <input type="email" className="input" placeholder="Email" />
                 <label className="label">Password</label>
@@ -28,6 +35,7 @@ const HeroRegister = () => {
                 </div>
                 <button className="btn btn-neutral mt-4">Login</button>
               </fieldset>
+           </form>
             </div>
           </div>
         </div>
