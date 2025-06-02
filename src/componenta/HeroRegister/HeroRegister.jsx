eccef43dff047408efc7d14b46ed3fb1610dffee
Alal Hosen <alalhosen1992620120@gmail.com>
2025-06-02 22:24:59 +0600

    name input add

--- src/componenta/HeroRegister/HeroRegister.jsx
+++ src/componenta/HeroRegister/HeroRegister.jsx
@@ -1,13 +1,11 @@
 const HeroRegister = () => {
-
-
-const handleRegister = e => {
+  const handleRegister = (e) => {
     e.preventDefault();
-    console.log('form submitted')
+    console.log("form submitted");
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log(email, password);
-}
+  };
 
   return (
     <div>
@@ -23,22 +21,37 @@ const handleRegister = e => {
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-           <form onSubmit={handleRegister}>
-                 <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" name="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
-                <input
-                  type="password" name="password"
-                  className="input"
-                  placeholder="Password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
-                <button className="btn btn-neutral mt-4">Login</button>
-              </fieldset>
-           </form>
+              <form onSubmit={handleRegister}>
+                <fieldset className="fieldset">
+                  <label className="label">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="input"
+                    placeholder="Name"
+                  />
+
+                  <label className="label">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="input"
+                    placeholder="Email"
+                  />
+
+                  <label className="label">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+              </form>
             </div>
           </div>
         </div>
